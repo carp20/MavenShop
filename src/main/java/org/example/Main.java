5ff8add8d9cf83266ede3ff8f172d123280b6c4c
Monkey308 <Vladimir91731@yandex.ru>
2025-01-31 18:03:22 +0300

    Edit Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Здраствуйте наш покупатель\nПриветствую вас в нашем магазине Шкидос");
+
+        System.out.println("Вам доступен следующий список действий: 'Купить' что-то. Например вантуз.\nТакже вам доступна " +
+                "'Продажа'. Например тапков.\nТакже вам доступно' ' ");
 
     }
 }
